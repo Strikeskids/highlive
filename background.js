68bd3f916f035f3f4427ca2bb0f408dd0850753d
Strikeskids <github@strikeskids.com>
2016-01-22 21:00:03 -0500

    Basic logic outline

--- background.js
+++ background.js
@@ -7,5 +7,15 @@ chrome.webRequest.onResponseStarted.addListener(handlePageLoad,
 )
 
 function handlePageLoad(details) {
-    console.log(details.responseHeaders)
+    var contentType = getContentType(details)
+    if (contentType.startsWith('text/plain')) {
+        // Perform language detection
+        // Perform syntax highlighting
+    }
+}
+
+function getContentType(details) {
+    if (!details || !details.responseHeaders) return
+    var header = details.responseHeaders.find(header => header.name.toLowerCase() === 'content-type')
+    return header && header.value
 }
