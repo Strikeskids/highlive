87924570971dcf1f96c35b5924aec314ff65d7eb
Strikeskids <github@strikeskids.com>
2016-01-25 17:19:08 -0500

    Improve highlighter detection

--- highlighter.js
+++ highlighter.js
@@ -2,15 +2,41 @@ var sel = document.getSelection()
 
 if (sel && sel.anchorNode) {
     var node = sel.anchorNode
-    while (!node.tagName) {
+    while (node && !isHighlighted(node) && !canHighlight(node)) {
         node = node.parentElement
     }
-    if (node.tagName === 'PRE') {
-        var code = document.createElement('code')
-        while (node.firstChild) {
-            code.appendChild(node.firstChild)
+    if (canHighlight(node)) {
+        if (!hasCodeNode(node)) {
+            wrapInCode(node)
         }
-        node.appendChild(code)
-        hljs.highlightBlock(code)
+        hljs.highlightBlock(node)
     }
+}
+
+function hasCodeNode(node) {
+    if (node.firstChild.nodeType !== Node.TEXT_NODE) return true
+    while (node) {
+        if (node.nodeName.toLowerCase() === 'code') return true
+        node = node.parentElement
+    }
+    return false
+}
+
+function wrapInCode(node) {
+    var code = document.createElement('code')
+    while (node.firstChild) {
+        code.appendChild(node.firstChild)
+    }
+    node.appendChild(code)
+}
+
+function canHighlight(node) {
+    if (!node || !node.firstChild || !node.tagName) return false
+    var tagName = node.nodeName.toLowerCase()
+    return (tagName === 'code' || tagName === 'pre')
+        && !isHighlighted(node) && !canHighlight(node.parentElement)
+}
+
+function isHighlighted(node) {
+    return node.querySelector && !!node.querySelector('span')
 }
