ea5ea12690fadf65e71a1913d8d6669e590d68b8
Strikeskids <github@strikeskids.com>
2016-01-28 16:06:25 -0500

    Include language highlighted box

--- highlighter.js
+++ highlighter.js
@@ -6,11 +6,51 @@ if (sel && sel.anchorNode) {
         node = node.parentElement
     }
     if (canHighlight(node)) {
-        if (!hasCodeNode(node)) {
-            wrapInCode(node)
-        }
-        hljs.highlightBlock(node)
+        runHighlight(node)
     }
+}
+
+function runHighlight(node) {
+    var text = node.textContent
+    var oldClassName = node.className
+
+    var result = hljs.highlightAuto(text)
+
+    node.innerHTML = result.value
+    if (!hasCodeNode(node)) {
+        wrapInCode(node)
+    }
+    
+    node.className = buildClassName(oldClassName, result.language)
+    node.style.cssText += 'position: relative;'
+    addLanguageNode(node, result.language)
+}
+
+function buildClassName(className, language) {
+    var result = className
+    if (!result.match(/\bhljs\b/)) {
+        result += ' hljs'
+    }
+    if (result.indexOf(language) === -1) {
+        result += ' ' + language
+    }
+    return result
+}
+
+function addLanguageNode(node, language) {
+    var menu = document.createElement('div')
+    menu.style.cssText = (
+            'position: absolute;' +
+            'right: 0;' +
+            'top: 0;' +
+            'font-family: initial;' +
+            'white-space: initial;' +
+            'background-color: #f0fff0;' + 
+            'padding: 5px;' +
+            'border-bottom-left-radius: 5px;'
+        )
+    menu.innerText = language
+    node.appendChild(menu)
 }
 
 function hasCodeNode(node) {
